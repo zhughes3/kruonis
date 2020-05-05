09cd1e86b5f1a707109ce119ab2df6c996812373
Jan van Natijne <jan@maketek.nl>
2020-05-05 11:41:13 +0200

    Update event

--- frontend/src/Utils/PasswordChecks.ts
+++ frontend/src/Utils/PasswordChecks.ts
@@ -1,3 +1,6 @@
+// Changing this will change the accepted minimal password length, and also the error messages when shown when inputting a too short password.
+export const MIN_PASSWORD_LENGTH: number = 8;
+
 export const checkForEmptyPassword = (password: string): boolean => {
 	return !!password.length;
 }
@@ -7,5 +10,5 @@ export const checkIfPasswordsMatch = (password: string, repeatPassword: string):
 }
 
 export const checkForPasswordLength = (password: string): boolean => {
-	return password.length >= 8;
+	return password.length >= MIN_PASSWORD_LENGTH;
 }
