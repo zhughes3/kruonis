c55a5b0bacffb3602ec36a426619706f63c84274
Jan van Natijne <jan@maketek.nl>
2020-05-21 17:01:10 +0200

    Added comments to PrivateRoute.tsx

--- frontend/src/Components/PrivateRoute.tsx
+++ frontend/src/Components/PrivateRoute.tsx
@@ -2,6 +2,12 @@ import {Redirect, Route} from "react-router-dom";
 import React, {useEffect, useState} from "react";
 import {checkIfLoggedIn} from "../Http/Requests";
 
+/**
+ * @EXPLANATION
+ * This component checks if a user is logged in. If they are, send the user to the page they want to go to.
+ * If they are not, redirect that user back to the login page.
+ *
+ */
 // @ts-ignore
 export const PrivateRoute = ({ component, ...options }) => {
 
@@ -35,11 +41,11 @@ export const PrivateRoute = ({ component, ...options }) => {
 		return <div />
 	}
 
-	// If login check came back false.
+	// If login check came back false, send user to login. We also send the page of origin so we can redirect back to the private page after login.
 	if (!loggedIn && !loading) {
 		return <Redirect {...options} to={{pathname: "/login", state: { from: options.path }}} />;
 	}
 
-	// If login check came back true;
+	// If login check came back true, allow the user to go to the appropriate route.
 	return <Route {...options} component={component} />;
 };
