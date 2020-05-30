443d11cc5e1a8210614adfc66f19c384b9d2bf29
Jan van Natijne <jan@maketek.nl>
2020-05-30 11:41:56 +0200

    Idiot me forgot to push code in last commit. Also: Now displays message to user on Dashboard.tsx if user has no timelines.

--- frontend/src/Http/HttpSetup.ts
+++ frontend/src/Http/HttpSetup.ts
@@ -2,10 +2,15 @@ import {ReqMethod} from "../Interfaces/Enums/ReqMethod";
 
 const BASE_URL = 'http://localhost';
 const PORT = '8080';
+// This port is only used for posting images.
+const IMAGE_PORT = '8081';
 const API_BASE = 'v1';
 
-const makeUrl = (url: string): string => {
-    return `${BASE_URL}:${PORT}/${API_BASE}/${url}`;
+// This function created the URL used to communicate with the API.
+// The image boolean exists because it the images are posted to a different port then other content.
+// If you set images to true, you'll send data to a different port then if it is false.
+const makeUrl = (url: string, image?: boolean): string => {
+    return `${BASE_URL}:${image ? IMAGE_PORT : PORT}/${API_BASE}/${url}`;
 };
 
 const getReqConfig = (reqMethod: ReqMethod, body?: any): any => {
@@ -24,13 +29,17 @@ const getReqConfig = (reqMethod: ReqMethod, body?: any): any => {
 };
 
 export const httpGet = async (url: string): Promise<any> => {
+
     let response: Response = await fetch(makeUrl(url), getReqConfig(ReqMethod.GET));
+
     return await response.json();
 };
 
-export const httpPost = async (url: string, body: any): Promise<any> => {
+// If you want to post an image, just set image to true when calling this function. The right PORT will be set automatically.
+// Also, if you are sending an image, the body will not be stringified.
+export const httpPost = async (url: string, body: any, image?: boolean): Promise<any> => {
 
-    let response: Response = await fetch(makeUrl(url), getReqConfig(ReqMethod.POST, JSON.stringify(body)));
+    let response: Response = await fetch(makeUrl(url, image), getReqConfig(ReqMethod.POST, image ? body : JSON.stringify(body)));
 
     return await response.json();
 };
